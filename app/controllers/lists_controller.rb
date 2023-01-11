78927b49ea44421320552427c0ebaa02405e5c0e
alice-starmine11 <121926699+alice-starmine11@users.noreply.github.com>
2023-01-11 01:09:41 +0000

    [Add] 3-5

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -20,6 +20,13 @@ class ListsController < ApplicationController
   end
 
   def edit
+    @list = List.find(params[:id])
+  end
+
+  def update
+    list = List.find(params[:id])
+    list.update(list_params)
+    redirect_to list_path(list.id)
   end
 
   private
@@ -27,4 +34,5 @@ class ListsController < ApplicationController
   def list_params
     params.require(:list).permit(:title, :body)
   end
+
 end
